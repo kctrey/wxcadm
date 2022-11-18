85f9ff1241c752a1098d9b9178dd684d406cb412
Trey <kctrey@gmail.com>
2022-11-18 11:20:46 -0600

    POST logging enhancement

--- wxcadm/common.py
+++ wxcadm/common.py
@@ -153,6 +153,7 @@ def webex_api_call(method: str,
                 else:
                     raise APIError(f"The Webex API returned an error: {r.text}")
         elif method.lower() == "post":
+            log.debug(f"Post body: {payload}")
             r = session.post(url_base + url, params=params, json=payload)
             if r.ok:
                 try:
@@ -166,7 +167,7 @@ def webex_api_call(method: str,
                     log.debug(f"__webex_api_call() completed in {end - start} seconds")
                     return response
             else:
-                log.warning("Webex API returned an error")
+                log.warning(f"Webex API returned an error: {r.text}")
                 if r.status_code == 429:
                     retry_after = int(r.headers.get('Retry-After', 30))
                     log.info(f"Received 429 Too Many Requests. Waiting {retry_after} seconds to retry.")
