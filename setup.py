098ea25a8f4db6fa1e66c50e4c0987b10083d8cf
Trey <kctrey@gmail.com>
2024-07-25 09:07:32 -0500

    rel:4.3.3 final

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='wxcadm',
-    version='4.3.1',
+    version='4.3.3',
     packages=packages,
     url='https://github.com/kctrey/wxcadm',
     license='GPL-3.0',
