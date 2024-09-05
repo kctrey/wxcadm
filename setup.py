29ce94c84902c3c5eaccac7c8c02166ea2bd6cf3
Trey <kctrey@gmail.com>
2024-09-05 11:02:34 -0500

    rel:4.3.6 with Xsi bug fix

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='wxcadm',
-    version='4.3.5',
+    version='4.3.6',
     packages=packages,
     url='https://github.com/kctrey/wxcadm',
     license='GPL-3.0',
