ded0f11eb528f4b2d6f15a14ffe9f3009b96d4d0
trey <kctrey@gmail.com>
2025-01-24 10:42:46 -0600

    chore:Added debug logging to RealtimeClass

--- wxcadm/realtime.py
+++ wxcadm/realtime.py
@@ -11,9 +11,9 @@ from .exceptions import PutError
 class RealtimeClass:
     def __init__(self):
         data: dict = webex_api_call('get', self.data_url)
-        log.debug(f"Received {data}")
+        log.debug(f"[RealTime] Received {data}")
         for key, val in data.items():
-            log.debug(f"Setting {key} to {val}")
+            log.debug(f"[RealTime] Setting {key} to {val}")
             self.__setattr__(humps.decamelize(key), val)
             self._api_fields.append(humps.decamelize(key))
         self._initialized = True
@@ -22,21 +22,21 @@ class RealtimeClass:
         return_dict = {}
         for field in self._api_fields:
             value = self.__getattribute__(field)
-            log.debug(f"Adding {field} to payload")
+            log.debug(f"[RealTime] Adding {field} to payload")
             return_dict[humps.camelize(field)] = value
-        log.debug(f"Returning {return_dict}")
+        log.debug(f"[RealTime] Returning {return_dict}")
         return return_dict
 
     def __setattr__(self, name, value):
         if self._initialized:
             old_val = self.__getattribute__(name)
-            log.info(f"Changing {name} from {old_val} to {value}")
+            log.info(f"[RealTime] Changing {name} from {old_val} to {value}")
             object.__setattr__(self, name, value)
             try:
                 webex_api_call('put', self.data_url,
                                payload=self.config_json())
             except wxcadm.exceptions.APIError as e:
-                log.warning("The change failed.")
+                log.warning("[RealTime] The change failed.")
                 object.__setattr__(self, name, old_val)
                 raise PutError(e)
         else:
